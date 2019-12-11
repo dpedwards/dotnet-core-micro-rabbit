12d1c86d31a0cf8712a3d42278f491db1f767028
Davain Pablo Edwards <core8@gmx.net>
2019-12-11 17:58:39 +0100

    :hatching_chick: Initial commit of infrastructure Dependency project

---
+++ MicroRabbit/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace MicroRabbit.Infra.IoC
+{
+
+   /**
+    * Dependency Container class 
+    * 
+    * @author D. P. Edwards
+    * @license MIT
+    * @version 1.0
+    */
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+
+    /**
+     * Dependency Container class 
+     * 
+     * @author D. P. Edwards
+     * @license MIT
+     * @version 1.0
+     */
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
