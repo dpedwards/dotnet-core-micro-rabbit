3566bb44749ac4a100a8eaab92c48b46e0969c73
Davain Pablo Edwards <core8@gmx.net>
2019-12-12 13:58:13 +0100

    :hatched_chick:  completely implementation of  MVC Communication to Banking Microservice project

---
+++ MicroRabbit/MicroRabbit.MVC/Services/ITransferService.cs
@@ -0,0 +1,17 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.MVC.Services
+{
+    /**
+    * Transfer Service interface
+    * 
+    * @author D. P. Edwards
+    * @license MIT
+    * @version 1.0
+    */
+    public interface ITransferService
+    {
+        Task Transfer(TransferDto transferDto);
+    }
+}
---
+++ MicroRabbit/MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using MicroRabbit.MVC.Models.DTO;
+using Newtonsoft.Json;
+
+namespace MicroRabbit.MVC.Services
+{
+    /**
+     * Transfer Service class
+     * 
+     * Local proxy banking service.
+     * 
+     * @author D. P. Edwards
+     * @license MIT
+     * @version 1.0
+     */
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _apiClient;
+
+        public TransferService(HttpClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task Transfer(TransferDto transferDto)
+        {
+            var uri = "https://localhost:5001/api/Banking";
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transferDto),
+                                            System.Text.Encoding.UTF8, "application/json");
+            var response = await _apiClient.PostAsync(uri, transferContent);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
